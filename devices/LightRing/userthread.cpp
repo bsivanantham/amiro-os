ebdce0e55e58baab85aea052859e7e74761cf7e9
mohith bhargav sunkara <msunkara@techfak.uni-bielefeld.de>
2018-11-23 20:53:01 +0100

    move robot

--- devices/LightRing/userthread.cpp
+++ devices/LightRing/userthread.cpp
@@ -10,6 +10,7 @@
 using namespace amiro;
 
 extern Global global;
+types::kinematic kinematic;
 
 UserThread::UserThread() :
   chibios_rt::BaseStaticThread<USER_THREAD_STACK_SIZE>()
@@ -40,15 +41,22 @@ UserThread::main()
     /*
      * INSERT CUSTOM CODE HERE
      */
-    // chprintf((BaseSequentialStream*)&global.sercanmux1,"checking \n");
-    i2sStartExchange(&I2SD2);
-    for (size_t i = 0; i < I2S_BUF_SIZE; i++) {
-      // chprintf((BaseSequentialStream*)&global.sercanmux1,"%d i2s_rx_f[%d]: 0x%08X \n",I2S_BUF_SIZE, i, global.i2s_rx_buf[i]);
-      chprintf((BaseSequentialStream*)&global.sercanmux1,"0x%08X,%d,%d\n",global.i2s_rx_buf[i], global.i2s_rx_buf[i],i);
-    }
+     chprintf((BaseSequentialStream*)&global.sercanmux1,"ROBOT MOVING \n");
+     kinematic = {10000000, 10000000, 10000000, 10000000, 10000000, 10000000};
 
-    i2sStopExchange(&I2SD2);
-    this->sleep(MS2ST(100000));
+     chprintf((BaseSequentialStream*)&global.sercanmux1,"Starting \n");
+     this->sleep(MS2ST(5000));
+     i2sStartExchange(&I2SD2);
+     for (size_t i = 1; i < I2S_BUF_SIZE; i+=2) {
+       // global.robot.setTargetSpeed(global.i2s_rx_buf[i],global.i2s_rx_buf[i]);
+       // this->sleep(MS2ST(500));
+      chprintf((BaseSequentialStream*)&global.sercanmux1,"%d i2s_rx_f[%d]: 0x%08X \n",I2S_BUF_SIZE, i, global.i2s_rx_buf[i]);
+     //  chprintf((BaseSequentialStream*)&global.sercanmux1,"%d,%d\n", global.i2s_rx_buf[i],i);
+     }
+
+     i2sStopExchange(&I2SD2);
+     chprintf((BaseSequentialStream*)&global.sercanmux1,"Stoping \n");
+     this->sleep(MS2ST(5000));
 
   }
 
