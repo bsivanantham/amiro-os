61315f24d499898f6dca776fde0f54d629c6d4a7
mohith bhargav sunkara <msunkara@techfak.uni-bielefeld.de>
2019-01-15 13:52:49 +0100

    application working

--- devices/LightRing/userthread.cpp
+++ devices/LightRing/userthread.cpp
@@ -30,10 +30,30 @@ UserThread::~UserThread()
 {
 }
 
+void light_on_n_m(int from, int to, const Color& color){
+  for(int i = from; i <= to; i++){
+    global.robot.setLightColor(i, Color(color));
+  }
+}
+
+void light_off_n(int n){
+  for(int i = 7; i >= 8-n; i--){
+    global.robot.setLightColor(i, Color(Color::BLACK));
+  }
+}
+
 void fourier_analysis() {
   chprintf((BaseSequentialStream*)&global.sercanmux1,"i, k, frequency, d1, d1, i2s_fft_buf[k]\n");
   double PI = 3.141592653589793238460;
+  bool color_switch = false;
   for (size_t i = 1, k = 0; i < I2S_BUF_SIZE; i = i + 2) {
+    if(color_switch){
+      color_switch = false;
+      light_on_n_m(0, 7, Color::DARKRED);
+    } else {
+      color_switch = true;
+      light_on_n_m(0, 7, Color::BLUE);
+    }
     uint32_t raw1 = global.i2s_rx_buf[i] /*& 0x0000FFFF*/;
     int16_t d1 = raw1 & 0x0000FFFF;
 
@@ -64,31 +84,33 @@ void fourier_analysis() {
     k++;
     //chprintf((BaseSequentialStream*)&global.sercanmux1,"%d,%d\n", i, d1);
   }
+  light_off_n(8);
 }
 
-void light_on(int from, int to, UserThread& u,const Color& color){
-  chprintf((BaseSequentialStream*)&global.sercanmux1,"Light on \n");
+void light_on_n_m_100ms(int from, int to, UserThread& u,const Color& color){
   for(int i = from; i <= to; i++){
-    chprintf((BaseSequentialStream*)&global.sercanmux1,"%d \n", i);
     u.sleep(MS2ST(100));
     global.robot.setLightColor(i, Color(color));
   }
 }
 
-void light_off(int n, UserThread& u){
-  chprintf((BaseSequentialStream*)&global.sercanmux1,"Light off \n");
+void light_off_n_100ms(int n, UserThread& u){
   for(int i = 7; i >= 8-n; i--){
-    chprintf((BaseSequentialStream*)&global.sercanmux1,"%d \n", i);
     u.sleep(MS2ST(100));
     global.robot.setLightColor(i, Color(Color::BLACK));
   }
 }
 
 void light_show(UserThread& u){
-  chprintf((BaseSequentialStream*)&global.sercanmux1,"in light up \n");
-  for (size_t i = 1; i < I2S_fft_BUF_SIZE; i++) {
-    chprintf((BaseSequentialStream*)&global.sercanmux1,"in for loop \n");
+  for (size_t i = 0; i < I2S_fft_BUF_SIZE; i++) {
     double abs_fft = i2s_fft_buf[i];
+    int32_t highWheelSpeed = 100000000;
+    int32_t lowWheelSpeed = 60000000;
+    if(i == 499){
+      chprintf((BaseSequentialStream*)&global.sercanmux1,"go backward \n");
+      highWheelSpeed *= -1;
+      lowWheelSpeed *= -1;
+    }
 
     // set the front LEDs to blue for one second
     // global.robot.setLightColor(constants::LightRing::LED_SSW, Color(Color::WHITE));
@@ -99,104 +121,74 @@ void light_show(UserThread& u){
     // global.robot.setLightColor(constants::LightRing::LED_ENE, Color(Color::WHITE));
     // global.robot.setLightColor(constants::LightRing::LED_ESE, Color(Color::WHITE));
     // global.robot.setLightColor(constants::LightRing::LED_SSE, Color(Color::WHITE));
-
-    if(abs_fft > 5000){
-      chprintf((BaseSequentialStream*)&global.sercanmux1,"more than 5000 amplitude \n");
+    if(abs_fft > 2000){
+      // chprintf((BaseSequentialStream*)&global.sercanmux1,"more than 5000 amplitude \n");
       double frequency = (32000/(double)(I2S_fft_BUF_SIZE-1)) * (double)i;
       int light_no = 0;
       if(frequency > 0 && frequency <= 4000){
-        light_off(7, u);
-        light_on(light_no,0,u,Color::GREEN);
+        light_off_n_100ms(7, u);
+        light_on_n_m_100ms(light_no,0,u,Color::GREEN);
+        global.robot.setTargetSpeed(highWheelSpeed,lowWheelSpeed);
         light_no = 0;
         // u.sleep(MS2ST(100));
         chprintf((BaseSequentialStream*)&global.sercanmux1,"more than 0 \n");
-        // global.robot.setLightColor(0, Color(Color::GREEN));
       }
       if(frequency > 4000){
-        light_off(6, u);
-        light_on(light_no,1,u,Color::GREEN);
+        light_off_n_100ms(6, u);
+        light_on_n_m_100ms(light_no,1,u,Color::GREEN);
+        global.robot.setTargetSpeed(lowWheelSpeed,highWheelSpeed);
         light_no = 1;
-        // u.sleep(MS2ST(100));
-        // chprintf((BaseSequentialStream*)&global.sercanmux1,"more than 4k \n");
-        // global.robot.setLightColor(0, Color(Color::GREEN));
-        // global.robot.setLightColor(1, Color(Color::GREEN));
       }
       if(frequency > 8000){
-        light_off(5, u);
-        light_on(light_no,2,u,Color::GREEN);
+        light_off_n_100ms(5, u);
+        light_on_n_m_100ms(light_no,2,u,Color::GREEN);
+        global.robot.setTargetSpeed(highWheelSpeed,lowWheelSpeed);
         light_no = 2;
         // u.sleep(MS2ST(100));
-        // chprintf((BaseSequentialStream*)&global.sercanmux1,"more than 8k \n");
-        // global.robot.setLightColor(0, Color(Color::GREEN));
-        // global.robot.setLightColor(1, Color(Color::GREEN));
-        // global.robot.setLightColor(2, Color(Color::GREEN));
+        chprintf((BaseSequentialStream*)&global.sercanmux1,"more than 8k \n");
       }
       if(frequency > 12000){
-        light_off(4, u);
-        light_on(light_no,3,u,Color::GREEN);
+        light_off_n_100ms(4, u);
+        light_on_n_m_100ms(light_no,3,u,Color::GREEN);
+        global.robot.setTargetSpeed(lowWheelSpeed,highWheelSpeed);
         light_no = 3;
         // u.sleep(MS2ST(100));
-        // chprintf((BaseSequentialStream*)&global.sercanmux1,"more than 12k \n");
-        // global.robot.setLightColor(0, Color(Color::GREEN));
-        // global.robot.setLightColor(1, Color(Color::GREEN));
-        // global.robot.setLightColor(2, Color(Color::GREEN));
-        // global.robot.setLightColor(3, Color(Color::GREEN));
+        chprintf((BaseSequentialStream*)&global.sercanmux1,"more than 12k \n");
       }
       if(frequency > 16000){
-        light_off(3, u);
-        light_on(light_no,4,u,Color::GREEN);
+        light_off_n_100ms(3, u);
+        light_on_n_m_100ms(light_no,4,u,Color::GREEN);
+        global.robot.setTargetSpeed(highWheelSpeed,lowWheelSpeed);
         light_no = 4;
         // u.sleep(MS2ST(100));
         // chprintf((BaseSequentialStream*)&global.sercanmux1,"more than 16k \n");
-        // global.robot.setLightColor(0, Color(Color::GREEN));
-        // global.robot.setLightColor(1, Color(Color::GREEN));
-        // global.robot.setLightColor(2, Color(Color::GREEN));
-        // global.robot.setLightColor(3, Color(Color::GREEN));
-        // global.robot.setLightColor(4, Color(Color::GREEN));
       }
       if(frequency > 20000){
-        light_off(2, u);
-        light_on(light_no,5,u,Color::GREEN);
+        light_off_n_100ms(2, u);
+        light_on_n_m_100ms(light_no,5,u,Color::GREEN);
+        global.robot.setTargetSpeed(lowWheelSpeed,highWheelSpeed);
         light_no = 5;
         // u.sleep(MS2ST(100));
         // chprintf((BaseSequentialStream*)&global.sercanmux1,"more than 20k \n");
-        // global.robot.setLightColor(0, Color(Color::GREEN));
-        // global.robot.setLightColor(1, Color(Color::GREEN));
-        // global.robot.setLightColor(2, Color(Color::GREEN));
-        // global.robot.setLightColor(3, Color(Color::GREEN));
-        // global.robot.setLightColor(4, Color(Color::GREEN));
-        // global.robot.setLightColor(5, Color(Color::GREEN));
       }
       if(frequency > 24000){
-        light_off(1, u);
-        light_on(light_no,6,u,Color::GREEN);
+        light_off_n_100ms(1, u);
+        light_on_n_m_100ms(light_no,6,u,Color::GREEN);
+        global.robot.setTargetSpeed(highWheelSpeed,lowWheelSpeed);
         light_no = 7;
         // u.sleep(MS2ST(100));
         // chprintf((BaseSequentialStream*)&global.sercanmux1,"more than 24k \n");
-        // global.robot.setLightColor(0, Color(Color::GREEN));
-        // global.robot.setLightColor(1, Color(Color::GREEN));
-        // global.robot.setLightColor(2, Color(Color::GREEN));
-        // global.robot.setLightColor(3, Color(Color::GREEN));
-        // global.robot.setLightColor(4, Color(Color::GREEN));
-        // global.robot.setLightColor(5, Color(Color::GREEN));
-        // global.robot.setLightColor(6, Color(Color::GREEN));
       }
       if(frequency > 28000 && frequency <= 32000){
-        light_off(0, u);
-        light_on(light_no,7,u,Color::GREEN);
+        light_off_n_100ms(0, u);
+        light_on_n_m_100ms(light_no,7,u,Color::GREEN);
+        global.robot.setTargetSpeed(lowWheelSpeed,highWheelSpeed);
         light_no = 7;
         // u.sleep(MS2ST(100));
         // chprintf((BaseSequentialStream*)&global.sercanmux1,"more than 28k \n");
-        // global.robot.setLightColor(0, Color(Color::GREEN));
-        // global.robot.setLightColor(1, Color(Color::GREEN));
-        // global.robot.setLightColor(2, Color(Color::GREEN));
-        // global.robot.setLightColor(3, Color(Color::GREEN));
-        // global.robot.setLightColor(4, Color(Color::GREEN));
-        // global.robot.setLightColor(5, Color(Color::GREEN));
-        // global.robot.setLightColor(6, Color(Color::GREEN));
-        // global.robot.setLightColor(7, Color(Color::GREEN));
       }
     }
+    // chprintf((BaseSequentialStream*)&global.sercanmux1,"less than 5000 amplitude \n");
   }
 }
 
@@ -285,6 +277,7 @@ UserThread::main()
       }
       this->sleep(MS2ST(1000));
     }
+    light_off_n(8);
     chprintf((BaseSequentialStream*)&global.sercanmux1,"before stop DMA \n");
     i2sStopExchange(&I2SD2);
     chprintf((BaseSequentialStream*)&global.sercanmux1,"stop DMA -> sleep 1s \n");
@@ -294,11 +287,24 @@ UserThread::main()
     chprintf((BaseSequentialStream*)&global.sercanmux1,"stop I2S -> 1s \n");
     this->sleep(MS2ST(1000));
     chprintf((BaseSequentialStream*)&global.sercanmux1,"Cycle %d\n", cycle);
+
     fourier_analysis();
     cycle++;
+    //ligh show
     for(int i=0; i<5; i++){
-      // chprintf((BaseSequentialStream*)&global.sercanmux1,"sleep %ds \n",i+1);
-      this->sleep(MS2ST(1000));
+      light_show(*this);
+    }
+
+    //stop robot movement
+    global.robot.setTargetSpeed(0,0);
+
+    //sleep before restart recording
+    for(int i=0; i<5; i++){
+      light_off_n(8);
+      this->sleep(MS2ST(500));
+      light_on_n_m(0,7,Color::BISQUE);
+      this->sleep(MS2ST(500));
+      chprintf((BaseSequentialStream*)&global.sercanmux1,"%ds\n", i+1);
     }
   }
 
